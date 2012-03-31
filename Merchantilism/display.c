3424526c6be106de6cf1f86178badfefa4e6fe5e
JP Moral <jp.moral@teamcodeflux.com>
2012-03-31 11:12:27 +0800

    Moved display_player_profile() definition from main.c to display.c.

--- Merchantilism/display.c
+++ Merchantilism/display.c
@@ -7,3 +7,25 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "display.h"
+#include "Constants.h"
+#include "player_and_locations.h"
+
+void display_player_profile(PLAYER *player) {
+    
+    system("clear");
+    printf("PROFILE:\n");
+    printf("========================\n");
+    printf("CASH: %.2f\n", player->cash);
+    printf("INVESTMENT: %.2f\tINTEREST:%.2f\n", player->investment, INVESTMENT_INTEREST_RATE);
+    printf("DEBT: %.2f\tINTEREST:%.2f\n", player->debt,DEBT_INTEREST_RATE);
+    printf("NET BALANCE: %.2f\n", player->net_balance);
+    printf("INVENTORY:\n");
+    printf("LUMBER: %d\n",player->lumber);
+    printf("STONE: %d\n",player->stone);
+    printf("SILK: %d\n",player->silk);
+    printf("ORE: %d\n",player->ore);
+    printf("GEM: %d\n",player->gem);
+    printf("========================\n");
+}
--- Merchantilism/main.c
+++ Merchantilism/main.c
@@ -27,21 +27,3 @@ int main (int argc, const char * argv[]) {
     
     return 0;
 }
-
-void display_player_profile(PLAYER *player) {
-    
-    system("clear");
-    printf("PROFILE:\n");
-    printf("========================\n");
-    printf("CASH: %.2f\n", player->cash);
-    printf("INVESTMENT: %.2f\tINTEREST:%.2f\n", player->investment, INVESTMENT_INTEREST_RATE);
-    printf("DEBT: %.2f\tINTEREST:%.2f\n", player->debt,DEBT_INTEREST_RATE);
-    printf("NET BALANCE: %.2f\n", player->net_balance);
-    printf("INVENTORY:\n");
-    printf("LUMBER: %d\n",player->lumber);
-    printf("STONE: %d\n",player->stone);
-    printf("SILK: %d\n",player->silk);
-    printf("ORE: %d\n",player->ore);
-    printf("GEM: %d\n",player->gem);
-    printf("========================\n");
-}
