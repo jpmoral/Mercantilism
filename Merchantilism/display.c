6a9a1a968e213c508e64087c4637b2681587b15b
JP Moral <jp.moral@teamcodeflux.com>
2012-03-31 11:09:35 +0800

    Added display .h and .c.  These will handle printing text to the screen.

---
+++ Merchantilism/display.c
@@ -0,0 +1,9 @@
+//
+//  display.c
+//  Mercantilism
+//
+//  Created by JP Moral on 3/31/12.
+//  Copyright (c) 2012 Codeflux. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ Merchantilism/display.h
@@ -0,0 +1,14 @@
+//
+//  display.h
+//  Mercantilism
+//
+//  Created by JP Moral on 3/31/12.
+//  Copyright (c) 2012 Codeflux. All rights reserved.
+//
+
+#ifndef Mercantilism_display_h
+#define Mercantilism_display_h
+
+
+
+#endif
