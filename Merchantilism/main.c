50b919d934bfaa2661f0e48e44393d50e8717491
JP Moral <jp.moral@teamcodeflux.com>
2012-03-31 11:46:35 +0800

    Removed main_menu() function. Moved code into main().

--- Merchantilism/main.c
+++ Merchantilism/main.c
@@ -8,13 +8,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "Constants.h"
 #include "player_and_locations.h"
 #include "display.h"
 
-void main_menu();
+void game(void);
 
 int main (int argc, const char * argv[]) {
+    
+    char choice = '\0';
+    
+    display_main_menu();
+    
+    while (choice != 'P' && choice != 'H' && choice != 'E') {
+        choice = getchar();
+        choice = toupper(choice);
+    }
+        
+    switch(choice){
+        case 'P':
+            game();
+            break;
+        case 'H':
+            printf("help");
+            break;
+        case 'E':
+            printf("Goodbye!\n");
+        break;}
+    
+    return 0;
+}
+
+void game () {
     
     PLAYER *player = (PLAYER *)malloc(sizeof(PLAYER));
     LOCATION *red = (LOCATION *)malloc(sizeof(PLAYER)), *azure = (LOCATION *)malloc(sizeof(PLAYER)), *emerald = (LOCATION *)malloc(sizeof(PLAYER)), *grey = (LOCATION *)malloc(sizeof(PLAYER)), *pirate = (LOCATION *)malloc(sizeof(PLAYER));
@@ -22,8 +48,5 @@ int main (int argc, const char * argv[]) {
     
     initialize_player(player);
     initialize_locations(loc_array, NUMBER_OF_LOCATIONS);
-    
     display_player_profile(player);
-    
-    return 0;
 }
