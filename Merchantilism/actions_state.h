3a5b2d2517767c5e5447c21da783083012e40c61
JP Moral <jp.moral@teamcodeflux.com>
2012-04-01 00:40:24 +0800

    Added actions_state .h and .c to hold state enums and action functions.

---
+++ Merchantilism/actions_state.h
@@ -0,0 +1,14 @@
+//
+//  player_actions.h
+//  Mercantilism
+//
+//  Created by JP Moral on 3/31/12.
+//  Copyright (c) 2012 Codeflux. All rights reserved.
+//
+
+#ifndef Mercantilism_player_actions_h
+#define Mercantilism_player_actions_h
+
+
+
+#endif
