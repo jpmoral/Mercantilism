4ecfad9ee282575173613fb2bdace7f32d95c1c5
JP Moral <jp.moral@teamcodeflux.com>
2012-03-31 15:51:56 +0800

    Started implementation of enter_bank().

--- Merchantilism/main.c
+++ Merchantilism/main.c
@@ -111,6 +111,18 @@ void help(void) {
 
 void enter_bank(PLAYER *player) {
     
+    display_player_profile(player);
+    display_bank_actions_menu();
+    consume_newline();
+    char choice = '\0';
+    char *valid_choices = VALID_BANK_ACTIONS;
+    
+    while (!strpbrk(valid_choices, &choice)) {
+        printf("not valid");
+        choice = getchar();
+        choice = toupper(choice);
+    }
+    
 }
 
 void enter_location(PLAYER *player, LOCATION *location) {
