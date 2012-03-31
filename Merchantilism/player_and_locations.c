919e9bda9f0a6ccc6743b48470045016a3fa9e77
JP Moral <jp.moral@teamcodeflux.com>
2012-03-31 10:23:01 +0800

    Finished implementation of initialize_locations.

--- Merchantilism/player_and_locations.c
+++ Merchantilism/player_and_locations.c
@@ -59,4 +59,30 @@ void initialize_locations(LOCATION *loc_array[], int number_of_locations) {
     loc_array[2]->silk_price = arc4random()%(EMERALD_CITY_SILK_MAX_PRICE - EMERALD_CITY_SILK_MIN_PRICE + 1) + EMERALD_CITY_SILK_MIN_PRICE;
     loc_array[2]->ore_price = arc4random()%(EMERALD_CITY_ORE_MAX_PRICE - EMERALD_CITY_ORE_MIN_PRICE + 1) + EMERALD_CITY_ORE_MIN_PRICE;
     loc_array[2]->gem_price = arc4random()%(EMERALD_CITY_GEM_MAX_PRICE - EMERALD_CITY_GEM_MIN_PRICE + 1) + EMERALD_CITY_GEM_MIN_PRICE;
+    
+    loc_array[3]->name = GREY_CANYON;
+    loc_array[3]->lumber_available = arc4random()%(GREY_CANYON_LUMBER_MAX_RESTOCK - GREY_CANYON_LUMBER_MIN_RESTOCK + 1) + GREY_CANYON_LUMBER_MIN_RESTOCK;
+    loc_array[3]->stone_available = arc4random()%(GREY_CANYON_STONE_MAX_RESTOCK - GREY_CANYON_STONE_MIN_RESTOCK + 1) + GREY_CANYON_STONE_MIN_RESTOCK;
+    loc_array[3]->silk_available = arc4random()%(GREY_CANYON_SILK_MAX_RESTOCK - GREY_CANYON_SILK_MIN_RESTOCK + 1) + GREY_CANYON_SILK_MIN_RESTOCK;
+    loc_array[3]->ore_available = arc4random()%(GREY_CANYON_ORE_MAX_RESTOCK - GREY_CANYON_ORE_MIN_RESTOCK + 1) + GREY_CANYON_ORE_MIN_RESTOCK;
+    loc_array[3]->gem_available = arc4random()%(GREY_CANYON_GEM_MAX_RESTOCK - GREY_CANYON_GEM_MIN_RESTOCK + 1) + GREY_CANYON_GEM_MIN_RESTOCK;
+    
+    loc_array[3]->lumber_price = arc4random()%(GREY_CANYON_LUMBER_MAX_PRICE - GREY_CANYON_LUMBER_MIN_PRICE + 1) + GREY_CANYON_LUMBER_MIN_PRICE;
+    loc_array[3]->stone_price = arc4random()%(GREY_CANYON_STONE_MAX_PRICE - GREY_CANYON_STONE_MIN_PRICE + 1) + GREY_CANYON_STONE_MIN_PRICE;
+    loc_array[3]->silk_price = arc4random()%(GREY_CANYON_SILK_MAX_PRICE - GREY_CANYON_SILK_MIN_PRICE + 1) + GREY_CANYON_SILK_MIN_PRICE;
+    loc_array[3]->ore_price = arc4random()%(GREY_CANYON_ORE_MAX_PRICE - GREY_CANYON_ORE_MIN_PRICE + 1) + GREY_CANYON_ORE_MIN_PRICE;
+    loc_array[3]->gem_price = arc4random()%(GREY_CANYON_GEM_MAX_PRICE - GREY_CANYON_GEM_MIN_PRICE + 1) + GREY_CANYON_GEM_MIN_PRICE;
+    
+    loc_array[4]->name = PIRATE_COVE;
+    loc_array[4]->lumber_available = arc4random()%(PIRATE_COVE_LUMBER_MAX_RESTOCK - PIRATE_COVE_LUMBER_MIN_RESTOCK + 1) + PIRATE_COVE_LUMBER_MIN_RESTOCK;
+    loc_array[4]->stone_available = arc4random()%(PIRATE_COVE_STONE_MAX_RESTOCK - PIRATE_COVE_STONE_MIN_RESTOCK + 1) + PIRATE_COVE_STONE_MIN_RESTOCK;
+    loc_array[4]->silk_available = arc4random()%(PIRATE_COVE_SILK_MAX_RESTOCK - PIRATE_COVE_SILK_MIN_RESTOCK + 1) + PIRATE_COVE_SILK_MIN_RESTOCK;
+    loc_array[4]->ore_available = arc4random()%(PIRATE_COVE_ORE_MAX_RESTOCK - PIRATE_COVE_ORE_MIN_RESTOCK + 1) + PIRATE_COVE_ORE_MIN_RESTOCK;
+    loc_array[4]->gem_available = arc4random()%(PIRATE_COVE_GEM_MAX_RESTOCK - PIRATE_COVE_GEM_MIN_RESTOCK + 1) + PIRATE_COVE_GEM_MIN_RESTOCK;
+    
+    loc_array[4]->lumber_price = arc4random()%(PIRATE_COVE_LUMBER_MAX_PRICE - PIRATE_COVE_LUMBER_MIN_PRICE + 1) + PIRATE_COVE_LUMBER_MIN_PRICE;
+    loc_array[4]->stone_price = arc4random()%(PIRATE_COVE_STONE_MAX_PRICE - PIRATE_COVE_STONE_MIN_PRICE + 1) + PIRATE_COVE_STONE_MIN_PRICE;
+    loc_array[4]->silk_price = arc4random()%(PIRATE_COVE_SILK_MAX_PRICE - PIRATE_COVE_SILK_MIN_PRICE + 1) + PIRATE_COVE_SILK_MIN_PRICE;
+    loc_array[4]->ore_price = arc4random()%(PIRATE_COVE_ORE_MAX_PRICE - PIRATE_COVE_ORE_MIN_PRICE + 1) + PIRATE_COVE_ORE_MIN_PRICE;
+    loc_array[4]->gem_price = arc4random()%(PIRATE_COVE_GEM_MAX_PRICE - PIRATE_COVE_GEM_MIN_PRICE + 1) + PIRATE_COVE_GEM_MIN_PRICE;
 }
